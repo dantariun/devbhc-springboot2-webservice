96a4fc412f056746cae4a39f3005715f6bdc15f8
devbhc <pepper>
2021-10-18 18:02:41 +0900

    ver 1.0.1

---
+++ src/main/java/com/devbhc/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.devbhc.book.springboot.web.dto;
+
+import com.devbhc.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
